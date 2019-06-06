bdcab4a23aacc64b2b70213421fac4ec5f704c9c
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-06-06 12:28:08 +0200

    files added

---
+++ cache/views/dd/dda2a0a90ccddf9a3e59aa7062653ee3b8d7552a560ac070d76b2fc008ba3a22.php
@@ -0,0 +1,73 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* index.view.html */
+class __TwigTemplate_c2686815daed82fdc76cd5ba00c118612090f864cb471d875d0f4ae976d3bca2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "template.view.html";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("template.view.html", "index.view.html", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 3
+        echo "<h1>HOLA MUNDO</h1>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.view.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 3,  46 => 2,  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "index.view.html", "/Users/Pablo/Desktop/bootcampA4B/Proyectos/proyectoblog/templates/index.view.html");
+    }
+}
